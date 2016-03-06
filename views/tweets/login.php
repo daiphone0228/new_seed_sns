c4f52e326852633541f7dc573df840652426e51e
DaiPhone <daiphone0228@gmail.com>
2016-03-06 19:23:36 +0800

    login.php編集

--- views/tweets/login.php
+++ views/tweets/login.php
@@ -5,14 +5,27 @@
           <div class="form-group">
             <label class="col-sm-4 control-label">メールアドレス</label>
             <div class="col-sm-8">
+            <?php if (isset($_POST['email']) && !empty($_POST['email'])) { ?>
+              <input type="email" name="email" class="form-control" placeholder="例： seed@nex.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+            <?php } else { ?>
               <input type="email" name="email" class="form-control" placeholder="例： seed@nex.com" value="">
+            <?php } ?>
+              <?php if (isset($error['login']) && $error['login'] == 'blank') { ?>
+              <p class="error">* メールアドレスとパスワードをご記入下さい</p>
+              <?php } elseif (isset($error['login']) && $error['login'] == 'failed') { ?>
+              <p class="error">* ログインに失敗しました。正しくご記入下さい。</p>
+              <?php } ?>
             </div>
           </div>
           <!-- パスワード -->
           <div class="form-group">
             <label class="col-sm-4 control-label">パスワード</label>
             <div class="col-sm-8">
+              <?php if (isset($_POST['email']) && !empty($_POST['email'])) { ?>
+              <input type="password" name="password" class="form-control" placeholder="" value="<?php echo htmlspecialchars($_POST['password']); ?>">
+            <?php } else { ?>
               <input type="password" name="password" class="form-control" placeholder="" value="">
+            <?php } ?>
             </div>
           </div>
           <div class="form-group">
